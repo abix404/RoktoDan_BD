6b4284d905ebb83c55e7fa4881840b50d39f890f
Abid Muhammad <111692910+abix404@users.noreply.github.com>
2025-08-16 04:26:46 +0600

    update

--- roktodanbdweb/admin.py
+++ roktodanbdweb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Donor
 
 # Register your models here.
+
+@admin.register(Donor)
+class DonorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'blood_group', 'district', 'registration_date')
+    list_filter = ('blood_group', 'district')
+    search_fields = ('first_name', 'last_name', 'phone_number', 'email')
--- roktodanbdweb/views.py
+++ roktodanbdweb/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DonorRegistrationForm
+from .models import Donor
 
 def home(request):
     return render(request, 'home.html')
@@ -7,5 +9,18 @@ def home(request):
 def about_us(request):
     return render(request, 'about_us.html')
 
-def as_donor(request):
-    return render(request, 'registration/as_donor.html')
+
+def register_donor(request):
+    if request.method == 'POST':
+        form = DonorRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('registration_success')
+    else:
+        form = DonorRegistrationForm()
+
+    return render(request, 'register_donor.html', {'form': form})
+
+
+def registration_success(request):
+    return render(request, 'success.html')
