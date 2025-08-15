734e38ac9e163988267071f85b1a809093ab7969
Abid Muhammad <111692910+abix404@users.noreply.github.com>
2025-08-16 02:34:00 +0600

    update

--- roktodanbdweb/views.py
+++ roktodanbdweb/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 # Create your views here.
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def as_donor(request):
+    return render(request, 'registration/as_donor.html')
